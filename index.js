e0c7191239030fb0c8db3cfec1763d97b7749117
Magno Gouveia <owlflee@gmail.com>
2022-04-04 13:18:40 -0300

    Start using port os env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
 
-const port = os.env.PORT
+const port = process.env.PORT
 
 app.use(bodyParser.json())
 
